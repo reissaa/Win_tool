a734dd7895fca62cfb3f46a0a4906fd8b7a30ac3
reissaa <matsunaga120613@gmail.com>
2025-07-20 22:52:26 +0900

    Create Environment_Analysis.py

---
+++ pages/Environment_Analysis.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from math import pi
+import folium
+from streamlit_folium import folium_static
+import plotly.express as px
+import altair as alt
+from PIL import Image
+import os
