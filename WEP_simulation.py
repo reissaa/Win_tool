eb9fd6b0118762010e1df51b28a0317ca6199ba8
reissaa <matsunaga120613@gmail.com>
2025-07-22 09:39:30 +0900

    Update WEP_simulation.py

--- WEP_simulation.py
+++ WEP_simulation.py
@@ -82,7 +82,7 @@ with col1:
     color=['crimson','darkorange','blue','darkgreen']
     fig, ax = plt.subplots()
     rate=[S_area,E_area,N_area,W_area]
-    Pie=ax.pie(rate,colors=color,startangle=90)
+    ax.pie(rate,colors=color,startangle=90)
     st.plotly_chart()
 
 col1, col2, col3, col4 = st.columns(4)
