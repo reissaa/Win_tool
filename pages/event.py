582265a3f7728c39c80fbd5b93ea0a3f7ae913e5
reissaa <matsunaga120613@gmail.com>
2025-11-24 21:30:21 +0900

    Add scatter plot for window performance data
    Load window performance data and create a scatter plot.

--- pages/event.py
+++ pages/event.py
@@ -26,3 +26,13 @@ with st.expander('Plot'):
 # Select other Plotly events by specifying kwargs
 #fig = px.line(x=[1], y=[1])
 #selected_points = plotly_events(fig, click_event=False, hover_event=True)
+Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv', header=0,sep=',',engine='python',encoding='cp932')
+fig = make_subplots()
+fig.add_trace(go.Scatter(x=list(Win_df['熱貫流率']), y=list(Win_df['日射熱取得率']),mode="markers"))
+fig.update_yaxes(range=(0,1))
+fig.update_yaxes(tick0=0,dtick=0.1)
+fig.update_xaxes(range=(0,6.0+0.11))
+fig.update_xaxes(tick0=0,dtick=0.5)
+#st.plotly_chart(fig, template=‘plotly_white’)
+selected_click = plotly_events(fig, click_event=True,hover_event=False,select_event=False,override_height=450,override_width='100%',key=None,)
+#st.write("選択された点:", Win_df['熱貫流率'])
