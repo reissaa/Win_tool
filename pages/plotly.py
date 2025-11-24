00b29a04f79185f4af6a42a18f0b64db63ab2a6b
reissaa <matsunaga120613@gmail.com>
2025-11-24 21:35:18 +0900

    Update plotly.py

--- pages/plotly.py
+++ pages/plotly.py
@@ -3,7 +3,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from streamlit_plotly_events import plotly_events
 import pandas as pd
-Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv',index_col=0, header=0,sep=',',engine='python',encoding='cp932')
+Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv',index_col=1, header=0,sep=',',engine='python',encoding='cp932')
 fig = make_subplots()
 fig.add_trace(go.Scatter(x=list(Win_df['熱貫流率']), y=list(Win_df['日射熱取得率']),mode="markers"))
 fig.update_yaxes(range=(0,1))
