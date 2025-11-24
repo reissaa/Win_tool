61bb0080f86f9dd1e92840ee387bb8ffa73dae7b
reissaa <matsunaga120613@gmail.com>
2025-11-24 22:14:30 +0900

    Update plotly.py

--- pages/plotly.py
+++ pages/plotly.py
@@ -1,9 +1,20 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import glob
+import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from streamlit_plotly_events import plotly_events
-import pandas as pd
-Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv',index_col=0, header=0,sep=',',engine='python',encoding='cp932')
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.io as pio
+location=os.listdir(R'site_info/')
+select_location =st.sidebar.selectbox('地域を選択してください', location)
+
+Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv', header=0,sep=',',engine='python',encoding='cp932')
+df1=pd.read_csv(f'WEP_Result47_4dir/{select_location}/direct_select/S/WEP_Result_{select_location}_S.csv', header=0,sep=',',engine='python'))
 fig = make_subplots()
 fig.add_trace(go.Scatter(x=list(Win_df['熱貫流率']), y=list(Win_df['日射熱取得率']),mode="markers"))
 fig.update_yaxes(range=(0,1))
