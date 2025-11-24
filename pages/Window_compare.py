e08bb363e4de2c8da40186af001da872027f9643
reissaa <matsunaga120613@gmail.com>
2025-11-25 01:02:25 +0900

    Update Window_compare.py

--- pages/Window_compare.py
+++ pages/Window_compare.py
@@ -116,5 +116,6 @@ ResultH1=pd.DataFrame({'Name':Name1 , '方位':x,'WEP_H':y1})
 ResultH2=pd.DataFrame({'Name':Name2 , '方位':x,'WEP_H':y2})
 ResultH=pd.concat([ResultH1,ResultH2],axis=0)
 st.write(ResultH)
-fig = px.bar(ResultH, x='方位', y='WEP_H',color='Name', barmode='group')
+Color1=['red','orange']
+fig = px.bar(ResultH, x='方位', y='WEP_H',color=Color1, barmode='group')
 st.plotly_chart(fig)
