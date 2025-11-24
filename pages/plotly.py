b7a9e41b50b34af53a09061fa9f73fdfa0577c8b
reissaa <matsunaga120613@gmail.com>
2025-11-24 22:44:14 +0900

    Update plotly.py

--- pages/plotly.py
+++ pages/plotly.py
@@ -35,19 +35,3 @@ dfS=pd.read_csv(f'WEP_Result47_4dir/{select_location}/direct_select/S/WEP_Result
 dfS.reset_index(inplace=True, drop=True)
 dfS.columns=['WEP_H','WEP_C']
 S_df=pd.concat([Win_df,dfS],axis=1)
-fig = px.scatter(S_df,x=S_df['熱貫流率'],y=S_df['日射熱取得率'],hover_name='窓の種類',color='日射熱取得率',hover_data='熱貫流率')
-fig.update_yaxes(range=(0,1))
-fig.update_yaxes(tick0=0,dtick=0.1)
-fig.update_xaxes(range=(0,6.0+0.11))
-fig.update_xaxes(tick0=0,dtick=0.5)
-fig.update_layout(
-    title="窓の熱貫流率と日射取得率",
-    xaxis_title="熱貫流率[W/㎡・K]",  
-    yaxis_title="日射取得率[-]",
-   width=700,
-    height=500,
-    font=dict(size=26,
-                color='grey'),
-    
-)
-st.plotly_chart(fig)
