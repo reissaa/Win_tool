2e9e46ba31dec919840ceb6ce0695496904abac9
reissaa <matsunaga120613@gmail.com>
2025-11-24 22:16:51 +0900

    Update plotly.py

--- pages/plotly.py
+++ pages/plotly.py
@@ -14,5 +14,22 @@ location=os.listdir(R'site_info/')
 select_location =st.sidebar.selectbox('地域を選択してください', location)
 
 Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv', header=0,sep=',',engine='python',encoding='cp932')
+fig =px.scatter(Win_df,x=Win_df["熱貫流率"],y=Win_df["日射熱取得率"],hover_name='窓の種類')
+
+fig.update_yaxes(range=(0,1))
+fig.update_yaxes(tick0=0,dtick=0.1)
+fig.update_xaxes(range=(0,6.0+0.11))
+fig.update_xaxes(tick0=0,dtick=0.5)
+fig.update_layout(
+    title="窓の熱貫流率と日射取得率",
+    xaxis_title="熱貫流率[W/㎡・K]",  
+    yaxis_title="日射取得率[-]",
+   width=700,
+    height=500,
+    font=dict(size=26,
+                color='grey'),
+    
+)
+st.plotly_chart(fig)
 df1=pd.read_csv(f'WEP_Result47_4dir/{select_location}/direct_select/S/WEP_Result_{select_location}_S.csv', header=0,sep=',',engine='python')
 
