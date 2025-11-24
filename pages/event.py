17bb58a3f7e36792cd73023c86a10b834cf123d3
reissaa <matsunaga120613@gmail.com>
2025-11-24 20:52:58 +0900

    Adjust y-axis and x-axis settings for scatter plot

--- pages/event.py
+++ pages/event.py
@@ -9,13 +9,13 @@ Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv', header=0,sep=',',engi
 # Writes a component similar to st.write()
 
 fig =px.scatter(Win_df, x="熱貫流率", y="日射熱取得率",hover_name='窓の種類')
-selected_points = plotly_events(fig, click_event=True, hover_event=True)
+
 fig.update_yaxes(range=(0,1))
 fig.update_yaxes(tick0=0,dtick=0.1)
 fig.update_xaxes(range=(0,6.0+0.11))
 fig.update_xaxes(tick0=0,dtick=0.5)
 #st.plotly_chart(fig)
-
+selected_points = plotly_events(fig, click_event=True, hover_event=True)
 #selected_points = plotly_events(fig)
 
 # Can write inside of things using with!
