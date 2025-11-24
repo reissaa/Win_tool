a0bb8ccb318312b9ab5416855b271e77db4ff330
reissaa <matsunaga120613@gmail.com>
2025-11-24 21:09:32 +0900

    Refactor event.py to streamline plotting logic

--- pages/event.py
+++ pages/event.py
@@ -7,16 +7,15 @@ import plotly.io as pio
 from streamlit_plotly_events import plotly_events
 Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv', header=0,sep=',',engine='python',encoding='cp932')
 # Writes a component similar to st.write()
-with st.expander('window-select'):
-    fig =px.scatter(Win_df,x="熱貫流率",y="日射熱取得率",hover_name='窓の種類')
+fig =px.scatter(Win_df,x="熱貫流率",y="日射熱取得率",hover_name='窓の種類')
 
-    fig.update_yaxes(range=(0,1))
-    fig.update_yaxes(tick0=0,dtick=0.1)
-    fig.update_xaxes(range=(0,6.0+0.11))
-    fig.update_xaxes(tick0=0,dtick=0.5)
-#st.plotly_chart(fig)
-    selected_points = plotly_events(fig, click_event=True, hover_event=True,key="event_page_plotly_selector")
-    st.write("選択された窓:", selected_points)
+fig.update_yaxes(range=(0,1))
+fig.update_yaxes(tick0=0,dtick=0.1)
+fig.update_xaxes(range=(0,6.0+0.11))
+fig.update_xaxes(tick0=0,dtick=0.5)
+st.plotly_chart(fig)
+#selected_points = plotly_events(fig, click_event=True, hover_event=True,key="event_page_plotly_selector")
+st.write("選択された窓:", selected_points)
 #selected_points = plotly_events(fig)
 
 # Can write inside of things using with!
