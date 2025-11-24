87f0ef805b99c79bd765d2d10d91532626f4f85a
reissaa <matsunaga120613@gmail.com>
2025-11-24 20:44:53 +0900

    Update event.py

--- pages/event.py
+++ pages/event.py
@@ -14,6 +14,7 @@ fig.update_yaxes(tick0=0,dtick=0.1)
 fig.update_xaxes(range=(0,6.0+0.11))
 fig.update_xaxes(tick0=0,dtick=0.5)
 st.plotly_chart(fig)
+selected_points = plotly_events(fig, click_event=False, hover_event=True)
 #selected_points = plotly_events(fig)
 
 # Can write inside of things using with!
