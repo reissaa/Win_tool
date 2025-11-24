bca13a2bd04b9df1f80e494ce6449595c45b6224
reissaa <matsunaga120613@gmail.com>
2025-11-24 22:15:48 +0900

    Update plotly.py

--- pages/plotly.py
+++ pages/plotly.py
@@ -15,12 +15,4 @@ select_location =st.sidebar.selectbox('地域を選択してください', locat
 
 Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv', header=0,sep=',',engine='python',encoding='cp932')
 df1=pd.read_csv(f'WEP_Result47_4dir/{select_location}/direct_select/S/WEP_Result_{select_location}_S.csv', header=0,sep=',',engine='python')
-fig = make_subplots()
-fig.add_trace(go.Scatter(x=list(Win_df['熱貫流率']), y=list(Win_df['日射熱取得率']),mode="markers"))
-fig.update_yaxes(range=(0,1))
-fig.update_yaxes(tick0=0,dtick=0.1)
-fig.update_xaxes(range=(0,6.0+0.11))
-fig.update_xaxes(tick0=0,dtick=0.5)
-#st.plotly_chart(fig, template=‘plotly_white’)
-selected_click = plotly_events(fig, click_event=True)
-st.write("選択された点:",selected_click)
+
