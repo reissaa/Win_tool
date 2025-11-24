90615c77c8577ac16ffd5b311885ec7cff31bac7
reissaa <matsunaga120613@gmail.com>
2025-11-24 20:02:40 +0900

    Update event.py

--- pages/event.py
+++ pages/event.py
@@ -1,17 +1,18 @@
 import plotly.express as px
 import streamlit as st
-
+import pandas as pd
+import numpy as np
 from streamlit_plotly_events import plotly_events
-
+df_S=pd.read_csv('WEP_Result47_4dir/TOKYO/direct_select/S/WEP_Result_TOKYO_S.csv', header=0,sep=',',engine='python')
 # Writes a component similar to st.write()
-fig = px.line(x=[1], y=[1])
+fig = px.scatter(df_S)
 selected_points = plotly_events(fig)
 
 # Can write inside of things using with!
-with st.expander('Plot'):
+#with st.expander('Plot'):
     fig = px.line(x=[1], y=[1])
     selected_points = plotly_events(fig, key="event_page_plotly_selector")
 
 # Select other Plotly events by specifying kwargs
-fig = px.line(x=[1], y=[1])
+#fig = px.line(x=[1], y=[1])
 selected_points = plotly_events(fig, click_event=False, hover_event=True)
