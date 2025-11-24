2f2e3b4e956dd582898cde32961f48cf0c06f68b
reissaa <matsunaga120613@gmail.com>
2025-11-24 21:19:01 +0900

    Update plotly.py

--- pages/plotly.py
+++ pages/plotly.py
@@ -2,10 +2,13 @@ import streamlit as st
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from streamlit_plotly_events import plotly_events
-
+Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv', header=0,sep=',',engine='python',encoding='cp932')
 fig = make_subplots()
-fig.add_trace(go.Scattergl(x=[1,2,3,4,5,6], y=[1,2,3,4,5,6]))
-fig.update_layout(width=1100)
+fig.add_trace(go.Scattergl(x=Win_df['熱貫流率'], y=Win_df['日射熱取得率']))
+fig.update_yaxes(range=(0,1))
+fig.update_yaxes(tick0=0,dtick=0.1)
+fig.update_xaxes(range=(0,6.0+0.11))
+fig.update_xaxes(tick0=0,dtick=0.5)
 #st.plotly_chart(fig, template=‘plotly_white’)
 selected_click = plotly_events(fig, click_event=True,hover_event=False,select_event=False,override_height=450,override_width='100%',key=None,)
 st.write("選択された点:", selected_click )
