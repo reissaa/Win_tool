4cfff93c5fe1138407ab2c4257814ba5e345b37b
reissaa <matsunaga120613@gmail.com>
2025-11-24 21:19:59 +0900

    Change Scattergl to Scatter for plotly trace

--- pages/plotly.py
+++ pages/plotly.py
@@ -5,7 +5,7 @@ from streamlit_plotly_events import plotly_events
 import pandas as pd
 Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv', header=0,sep=',',engine='python',encoding='cp932')
 fig = make_subplots()
-fig.add_trace(go.Scattergl(x=Win_df['熱貫流率'], y=Win_df['日射熱取得率']))
+fig.add_trace(go.Scatter(x=Win_df['熱貫流率'], y=Win_df['日射熱取得率']))
 fig.update_yaxes(range=(0,1))
 fig.update_yaxes(tick0=0,dtick=0.1)
 fig.update_xaxes(range=(0,6.0+0.11))
