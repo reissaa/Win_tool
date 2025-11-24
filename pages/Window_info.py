7b7bf254834836b5ac2a0c463d598678a519b5ec
reissaa <matsunaga120613@gmail.com>
2025-11-24 21:53:38 +0900

    Update Window_info.py

--- pages/Window_info.py
+++ pages/Window_info.py
@@ -10,7 +10,13 @@ import altair as alt
 from PIL import Image
 import os
 import plotly.graph_objects as go
-
+import plotly.express as px
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.io as pio
+from streamlit_plotly_events import plotly_events
 
 
 
@@ -27,15 +33,21 @@ df_0=pd.read_csv(R'Window_info/窓表作成.csv', header=0,sep=',',engine='pytho
 
 win_name=list(df_0['窓の種類'])
 
-col1, col2 = st.columns(2)
-img1=Image.open(Rf"png/熱貫流率の分布.png")
-img2=Image.open(Rf"png/日射熱取得率の分布.png")
-with col1:
-    st.header("断熱性能")
-    st.image(img1)
-with col2:
-   st.header("日射取得性能")
-   st.image(img2)
+Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv', header=0,sep=',',engine='python',encoding='cp932')
+# Writes a component similar to st.write()
+fig =px.scatter(Win_df,x=Win_df["熱貫流率"],y=Win_df["日射熱取得率"],hover_name='窓の種類')
+
+fig.update_yaxes(range=(0,1))
+fig.update_yaxes(tick0=0,dtick=0.1)
+fig.update_xaxes(range=(0,6.0+0.11))
+fig.update_xaxes(tick0=0,dtick=0.5)
+fig.update_layout(
+    title="窓の熱貫流率と日射取得率",
+    xaxis_title="熱貫流率[W/㎡・K]",  
+    yaxis_title="日射取得率[-]"
+)
+st.plotly_chart(fig)
+
 select_window=st.selectbox('窓を選択してください', win_name)
 st.header(f"窓の性能　選択された窓{select_window}")
     
