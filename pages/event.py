ccdafde03e7720f9d03eb6fb4b63fad633c18073
reissaa <matsunaga120613@gmail.com>
2025-11-24 20:49:44 +0900

    Update event.py

--- pages/event.py
+++ pages/event.py
@@ -8,7 +8,7 @@ from streamlit_plotly_events import plotly_events
 Win_df=pd.read_csv(R'窓性能ラベル/窓表作成.csv', header=0,sep=',',engine='python',encoding='cp932')
 # Writes a component similar to st.write()
 
-fig = px.scatter(Win_df, x="熱貫流率", y="日射熱取得率",hover_name='窓の種類')
+fig = st.scatter(Win_df, x="熱貫流率", y="日射熱取得率",hover_name='窓の種類')
 fig.update_yaxes(range=(0,1))
 fig.update_yaxes(tick0=0,dtick=0.1)
 fig.update_xaxes(range=(0,6.0+0.11))
