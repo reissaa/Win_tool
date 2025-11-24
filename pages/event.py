9b444a251709333840604ff0645fd751f89b42aa
reissaa <matsunaga120613@gmail.com>
2025-11-24 20:05:49 +0900

    Transpose DataFrame and rename columns
    Transpose the DataFrame and rename columns for clarity.

--- pages/event.py
+++ pages/event.py
@@ -5,6 +5,8 @@ import numpy as np
 from streamlit_plotly_events import plotly_events
 df_S=pd.read_csv('WEP_Result47_4dir/TOKYO/direct_select/S/WEP_Result_TOKYO_S.csv', header=0,sep=',',engine='python')
 # Writes a component similar to st.write()
+df_S=df_S.T
+df_S.columns=['WEP_H','WEP_C']
 fig = px.scatter(df_S)
 selected_points = plotly_events(fig)
 
