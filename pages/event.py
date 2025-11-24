bbd5ab90a88a11992b2ddbf6f2978139201dd1c9
reissaa <matsunaga120613@gmail.com>
2025-11-24 20:57:15 +0900

    Display selected points in event plot
    Add display of selected points in event plot.

--- pages/event.py
+++ pages/event.py
@@ -16,6 +16,7 @@ fig.update_xaxes(range=(0,6.0+0.11))
 fig.update_xaxes(tick0=0,dtick=0.5)
 #st.plotly_chart(fig)
 selected_points = plotly_events(fig, click_event=True, hover_event=True,key="event_page_plotly_selector")
+st.write("選択された窓:", selected_points)
 #selected_points = plotly_events(fig)
 
 # Can write inside of things using with!
