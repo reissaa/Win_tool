f3ab7302e1c4f037c69f62738d8b3c3dfb70d453
reissaa <matsunaga120613@gmail.com>
2025-11-25 09:12:37 +0900

    Update WEP_simulation.py

--- WEP_simulation.py
+++ WEP_simulation.py
@@ -54,29 +54,29 @@ map = folium.Map(location=point,zoom_start=4.7)
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    st.title('南面のWEPH')
-    st.markdown(f"{df_S[f'{select_winodow_s}'][0]:.2f}[kW/㎡・h]")
+    st.write('南面のWEPH')
+    st.markdown(f"{df_S[f'{select_winodow_s}'][0]:.2f}[kW/㎡]")
     st.title('南面のWEPC')
-    st.markdown(f"{df_S[f'{select_winodow_s}'][1]:.2f}[kW/㎡・h]")
+    st.markdown(f"{df_S[f'{select_winodow_s}'][1]:.2f}[kW/㎡]")
 
 
 with col2:
-    st.title('北面のWEPH')
-    st.markdown(f"{df_N[f'{select_winodow_n}'][0]:.2f}[kW/㎡・h]")
+    st.write('北面のWEPH')
+    st.markdown(f"{df_N[f'{select_winodow_n}'][0]:.2f}[kW/㎡]")
     st.title('北面のWEPC')
-    st.markdown(f"{df_N[f'{select_winodow_n}'][1]:.2f}[kW/㎡・h]")
+    st.markdown(f"{df_N[f'{select_winodow_n}'][1]:.2f}[kW/㎡]")
     
 with col3:
-    st.title('東面のWEPH')
-    st.markdown(f"{df_E[f'{select_winodow_e}'][0]:.2f}[kW/㎡・h]")
+    st.write('東面のWEPH')
+    st.markdown(f"{df_E[f'{select_winodow_e}'][0]:.2f}[kW/㎡]")
     st.title('東面のWEPC')
-    st.markdown(f"{df_E[f'{select_winodow_e}'][0]:.2f}[kW/㎡・h]")
+    st.markdown(f"{df_E[f'{select_winodow_e}'][0]:.2f}[kW/㎡]")
     
 with col4:
-    st.title('西面のWEPH')
-    st.markdown(f"{df_W[f'{select_winodow_w}'][0]:.2f}[kW/㎡・h]")
+    st.write('西面のWEPH')
+    st.markdown(f"{df_W[f'{select_winodow_w}'][0]:.2f}[kW/㎡]")
     st.title('西面のWEPC')
-    st.markdown(f"{df_W[f'{select_winodow_w}'][1]:.2f}[kW/㎡・h]")
+    st.markdown(f"{df_W[f'{select_winodow_w}'][1]:.2f}[kW/㎡]")
     
 
 WEP_Result=np.empty([2,4])
